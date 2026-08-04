bcb92e68f71c650983241fa4edbd65b5f2a1283e
DaveDaBrave <popescu.david.ioan@gmail.com>
2026-08-04 23:04:24 +0300

    back2manual

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        a = {}
-        for x in nums:
-            if x in a:
-                return True
-            a[x]=True
-        return False
-            
