a5b8c0d54ee4552ffaf826fdecdc5bf289d9ab45
pdavid-i <36517659+pdavid-i@users.noreply.github.com>
2026-08-04 23:47:22 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for st in strs: 
+            le_key = tuple(self.customHash(st))
+            if le_key in groups:
+                groups[le_key] = [*groups[le_key], st]
+            else:
+                groups[le_key] = [st]
+        return(list(groups.values()))
+
+    def customHash(self, to_hash: str) -> List[int]:
+        letter_hash = [0] * 26
+        for letter in to_hash:
+            letter_hash[ord(letter)-ord('a')]+=1
+        return letter_hash
